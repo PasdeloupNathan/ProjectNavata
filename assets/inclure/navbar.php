202affd0db90946dc1c79d3473bbe4a58756b727
PasdeloupNathan <gamee8678@gmail.com>
2020-08-17 11:45:25 +0200

    add nav meta

--- assets/inclure/navbar.php
+++ assets/inclure/navbar.php
@@ -1,32 +1,32 @@
 
 <?php
 
-template_header($title) {
-    echo <<<EOT
+function template_header($title) {
+echo <<<EOT
 
-    <div class="menu">
-        <div class="row haut align-items-center">
-            <div class="logo"><img src="../LOGO.png" alt="logo" class="logoimg"></div>
+<div class="menu">
+    <div class="row haut align-items-center">
+        <div class="logo"><img src="../LOGO.png" alt="logo" class="logoimg"></div>
+    </div>
+
+    <div class="row bas align-items-center">
+        <div class="col acceuil">
+            <h2 class="h2nav">Acceuil</h2>
+        </div>
+        <div class="col"></div>
+        <div class="col">
+            <h2 class="h2nav">Concours</h2>
+        </div>
+        <div class="col"></div>
+        <div class="col">
+            <h2 class="h2nav">Profil</h2>
         </div>
 
-        <div class="row bas align-items-center">
-            <div class="col acceuil">
-                <h2 class="h2nav">Acceuil</h2>
-            </div>
-            <div class="col"></div>
-            <div class="col">
-                <h2 class="h2nav">Concours</h2>
-            </div>
-            <div class="col"></div>
-            <div class="col">
-                <h2 class="h2nav">Profil</h2>
-            </div>
-
-        </div>
     </div>
-    <div class="bleu row align-items-center">
-        <p class="pnav"> Identifiez-vous (ou inscrivez-vous gratuitement) afin de profiter pleinement des fonctionnalités.</p>
-    </div>
+</div>
+<div class="bleu row align-items-center">
+    <p class="pnav"> Identifiez-vous (ou inscrivez-vous gratuitement) afin de profiter pleinement des fonctionnalités.</p>
+</div>
 
 EOT;
 }
