a7c1e0e37c5da76b3585b53c8dc8d6362e1bf878
Tarik Daouine <tarikdaouine@gmail.com>
2020-08-17 17:28:15 +0200

    coucou

--- concours.php
+++ concours.php
@@ -1,3 +1,12 @@
 <?php
 include 'meta.php';
 ?>
+<?=template_meta('meta')?>
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer('footer')?>
