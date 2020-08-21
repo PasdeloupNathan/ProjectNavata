26d0eb6690927f108506e02b8373ccd7dad2167d
PasdeloupNathan <gamee8678@gmail.com>
2020-08-21 16:20:22 +0200

    /

--- assets/php/index.php
+++ assets/php/index.php
@@ -8,9 +8,18 @@ include 'meta.php';
 <body class="body">
 
 <?=template_header()?>
-<div class="main1 row">
+<div class="main1 row" style="margin-left:0;margin-right:0;">
     
+<div class="col doubleC row align-items-center">
+    <div class="newC">
 
+    </div>
+</div>
+<div class="col doubleC row align-items-center">
+    <div class="endC">
+
+    </div>
+</div>
 
 
 </div>
