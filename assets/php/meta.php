de2c5743c7625b1dc85dfa7f0860ad1df0f56184
Tarik Daouine <tarikdaouine@gmail.com>
2020-09-01 14:31:03 +0200

    YO

--- assets/php/meta.php
+++ assets/php/meta.php
@@ -57,7 +57,24 @@ echo<<<EOT
 EOT;
 }
 
+function template_admin(){
+echo<<<EOT
+    
+        <div class="col- navGauche ">
+            <div class="  vertical-menu">
+                <a  class=" col- active">MENU</a>
+        <div class="menu1"><button class="but col-" >Acceuil </button></div>
+        <div class="menu1"><button class=" col-" >Concours</button></div>
+        <div class="menu1"><button class=" col-">Utilisateurs</button></div>
+        <div class="menu1"><button class=" col-">LOGS</button></div>
+        
+        </div>
+    </div>
+    
+    
 
+EOT;
+}
 
 function template_footer(){
 echo<<<EOT
