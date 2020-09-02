0d8125c79301007dc80db1e380ebd9ad7fd94514
Tarik Daouine <tarikdaouine@gmail.com>
2020-09-02 16:00:13 +0200

    modifi concours

--- assets/php/adminConcours.php
+++ assets/php/adminConcours.php
@@ -20,25 +20,29 @@ include 'meta.php';
 
 <form class="col-10 formulaire" action="">
     <div style="width: 100%; " class=" menu">
-        <div style="margin-top: 1%;" class="listeData row align-items-center">
-            <p class="offset-md-5 col-2">test</p>
-            <button class="col-2 update" type="button" name="modifier">modifier</button> 
-            <button class="col-2 delete" type="button" name="delete"> delete</button>
+        <div  class="listeData row align-items-center">
+            <div class="offset-md-5 col-2" ><p >test</p></div>
+            
+            <div  class="col-2"><button class="update" type="button" name="modifier">modifier</button> </div>
+            <div class="col-2 "><button class="delete" type="button" name="delete"> delete</button></div>
         </div>
-        <div style="margin-top: 1%;" class="listeData row align-items-center">
-            <p class="offset-md-5 col-2">test</p>
-            <button class="col-2 update" type="button" name="modifier">modifier</button> 
-            <button class="col-2 delete" type="button" name="delete"> delete</button>
+        <div  class="listeData row align-items-center">
+            <div class="offset-md-5 col-2" ><p > mus, ipsa id.</p></div>
+            
+            <div  class="col-2"><button class="update" type="button" name="modifier">modifier</button> </div>
+            <div class="col-2 "><button class="delete" type="button" name="delete"> delete</button></div>
         </div>
-        <div style="margin-top: 1%;" class="listeData row align-items-center">
-            <p class="offset-md-5 col-2">test</p>
-            <button class="col-2 update" type="button" name="modifier">modifier</button> 
-            <button class="col-2 delete" type="button" name="delete"> delete</button>
+        <div  class="listeData row align-items-center">
+            <div class="offset-md-5 col-2" ><p >test</p></div>
+            
+            <div  class="col-2"><button class="update" type="button" name="modifier">modifier</button> </div>
+            <div class="col-2 "><button class="delete" type="button" name="delete"> delete</button></div>
         </div>
-        <div style="margin-top: 1%;" class="listeData row align-items-center">
-            <p class="offset-md-5 col-2">test</p>
-            <button class="col-2 update" type="button" name="modifier">modifier</button> 
-            <button class="col-2 delete" type="button" name="delete"> delete</button>
+        <div  class="listeData row align-items-center">
+            <div class="offset-md-5 col-2" ><p >test</p></div>
+            
+            <div  class="col-2"><button class="update" type="button" name="modifier">modifier</button> </div>
+            <div class="col-2 "><button class="delete" type="button" name="delete"> delete</button></div>
         </div>
 <div style="width: 100%; " ><button class="col-3 validate" type="button">validé</button></div>
 
