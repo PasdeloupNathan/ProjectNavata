fd923e72d9abe218e89a6e2806478b958d529bd4
Tarik Daouine <60178432+Tarik-Daouine@users.noreply.github.com>
2020-10-11 16:21:13 +0200

    Update test.php

--- assets/php/test.php
+++ assets/php/test.php
@@ -8,19 +8,3 @@ $msg = '';
 
 <?php include('meta.php')?>
 <?=template_meta('test')?>
-
-<?php
-
-
-$teste = $pdo->prepare("SELECT * from users ");
-$teste->execute();
-$bro= $teste->fetchAll(\PDO::FETCH_ASSOC);
-
-?>
-<?php
-while($array= $teste);
-{
-  echo $array['prénoms'];
-}
-
-?>
