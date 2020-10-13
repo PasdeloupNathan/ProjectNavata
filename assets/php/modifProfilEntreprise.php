d25b35f86fb417b49e492c26b938cef98b19ef02
Valentin <gautias.gaming147@gmail.com>
2020-10-13 23:08:41 +0200

    commit

--- assets/php/modifProfilEntreprise.php
+++ assets/php/modifProfilEntreprise.php
@@ -72,7 +72,7 @@ foreach($societe as $societes)
 <?php 
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'projet_navata';
 
 
