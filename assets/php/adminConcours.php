5acadb577e279ac28f1e608bd874b91804f8e8b1
NathanPasdeloup <gamee8678@gmail.com>
2020-10-14 00:20:37 +0200

    .

--- assets/php/adminConcours.php
+++ assets/php/adminConcours.php
@@ -27,7 +27,7 @@ $annee = (int)date('Y');
 // Get the page via GET request (URL param: page), if non exists default the page to 1
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] :1;
 // Number of records to show on each page
-$records_per_page = 1;
+$records_per_page = 5;
 
 //  code pour la barre de recherche 
 if (isset($_GET['search'])) {
