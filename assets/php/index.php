dec64abd4369f6e1f06a7697afabc43026dfb068
NathanPasdeloup <gamee8678@gmail.com>
2020-10-14 02:55:36 +0200

    /

--- assets/php/index.php
+++ assets/php/index.php
@@ -11,7 +11,6 @@ $msg = '';
 
 
 <!-- // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion// -->
-
 <!-- Appeller de la page meta.php ou se situe toute les fonction template  -->
 <?php include 'meta.php';?>
 <!-- fonction permettant d'accédé a la page correspondant aux rôles -->
@@ -39,13 +38,6 @@ if($_SESSION == FALSE){
 
 <!-- sans avoir de compte utilisateur -->
 
-<?php 
-  $tests = $pdo->prepare("SELECT * from concours WHERE ref=".$_SESSION['id_entreprise']);
-  $tests->execute();
-  $societe= $tests->fetchAll(\PDO::FETCH_ASSOC);
-
-
-?>
 
 <?=template_meta('Acceuil')?>
 
--- assets/php/test2.php
+++ assets/php/test2.php
@@ -131,9 +131,7 @@ require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'function' . DIRECTORY_SEP
 
 
 </div>
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      
  <?=template_footer()?>
 
 </body>
